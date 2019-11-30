92340784b829faee7d9bc2f88e696c2fe3441aa2
schrodingerzhu <i@zhuyi.fan>
2019-11-30 20:09:20 +0800

    clean up

--- getcallpass.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tmpfilename=(date +%N)
-
-clang++ -emit-llvm -S $1 -o /tmp/$tmpfilename
-opt -load ./libcallpass.so -dumpcalls /tmp/$tmpfilename  >/dev/null #2>$2
-rm /tmp/$tmpfilename
